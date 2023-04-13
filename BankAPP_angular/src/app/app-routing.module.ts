79835bcad0b8dd8de85cfa7719b5350efae7514b
anaghaaa-r <r.anaghaaa@gmail.com>
2023-04-13 17:28:37 +0530

    creditcard routing

--- BankAPP_angular/src/app/app-routing.module.ts
+++ BankAPP_angular/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TransactionsComponent } from './transactions/transactions.component';
+import { CreditcardComponent } from './creditcard/creditcard.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'transactions', component: TransactionsComponent }
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'creditcard', component: CreditcardComponent }
 ];
 
 @NgModule({
