d5b3c3bee952c8f7f8fb0f9b898168f1cc41d637
anaghaaa-r <r.anaghaaa@gmail.com>
2023-04-13 17:07:13 +0530

    register component

--- BankAPP_angular/src/app/service/authservices.service.ts
+++ BankAPP_angular/src/app/service/authservices.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,71 @@ import { Injectable } from '@angular/core';
 })
 export class AuthservicesService {
 
-  constructor() { }
+  username: any
+  database: any =
+    {
+      1000: { acno: 1000, uname: "Vignesh", password: 1000, Balance: 10000 }
+    }
+  constructor(private http:HttpClient) { }
+  
+  register(acno: any, uname: any, password: any)
+  {
+    console.log("RESnameservice: ", acno, uname, password)
+    const data={
+      "acno": acno,
+      "uname": uname,
+      "password": password
+    }
+    return this.http.post("http://localhost:3007/register", data)
+  }
+    // var database: any=this.database
+    // if(acno in database)
+    // {
+    //   return false
+    // }
+    // else
+    // {
+    //   database[acno]=
+    //   {
+    //     acno,
+    //     uname,
+    //     "password": pswd,
+    //     Balance: 0
+    //   }
+    //   console.log("database", this.database)
+    // return true
+    // }
+
+  login(acno: any, password: any)
+  {
+    const data={
+      "acno": acno,
+      "password": password
+    }
+    return this.http.post("http://localhost:3007/login", data)
+  }
+  // {
+  //   let db=this.database
+
+  //   if(acno in db)
+  //   {
+  //     if(password=db[acno]['password'])
+  //     {
+  //       return true
+  //     }
+  //     else
+  //     {
+  //       alert("Incorrect Password")
+  //       return false
+  //     }
+  //   }
+  //   else
+  //   {
+  //     alert("Not a user, Register first")
+  //     return false
+  //   }
+  // }
 }
+
+
+
