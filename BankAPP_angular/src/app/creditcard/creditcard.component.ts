3e18195fe42cca6fffc29467406e53b543669f6b
anaghaaa-r <r.anaghaaa@gmail.com>
2023-04-13 17:24:25 +0530

    creditcard updated

--- BankAPP_angular/src/app/creditcard/creditcard.component.ts
+++ BankAPP_angular/src/app/creditcard/creditcard.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditcardComponent implements OnInit {
 
+  
+  FirstName: any
+  LastName: any
+  Age: any
+  PANCard: any
+  AccountNumber: any
+  AAdhar: any
+  dummyarray: any=[]
+  credDatabase: any={}
   constructor() { }
 
   ngOnInit(): void {
+  }
+  creditcardfun()
+  {
+    this.credDatabase[this.AccountNumber]=
+    {
+      "FirstName": this.FirstName,
+      "LastName": this.LastName,
+      "Age": this.Age,
+      "PanCard": this.PANCard,
+      "AccountNumber": this.AccountNumber,
+      "AAdhar": this.AAdhar
+    }
+
+    alert("Applied Successully")
+    console.log(this.credDatabase)
+
+    this.dummyarray.push
+    ({
+      "FirstName": this.FirstName,
+      "LastName": this.LastName,
+      "Age": this.Age,
+      "PanCard": this.PANCard,
+      "AccountNumber": this.AccountNumber,
+      "AAdhar": this.AAdhar
+    })
+    console.log(this.dummyarray)
   }
 
 }
