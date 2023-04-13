449359ec5df67ec3954ee57902d5912c1aac641e
anaghaaa-r <r.anaghaaa@gmail.com>
2023-04-13 17:27:01 +0530

    transactions routing

--- BankAPP_angular/src/app/app-routing.module.ts
+++ BankAPP_angular/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'transactions'  }
+  { path: 'transactions', component: TransactionsComponent  }
 ];
 
 @NgModule({
